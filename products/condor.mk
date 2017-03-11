9eb8d0d40153c76fd1f8d8538e8c6f699f671e7e
Govind Pathak <govindpathak122@gmail.com>
2017-03-11 21:26:31 +0530

    updated as instructed

--- products/condor.mk
+++ products/condor.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   DEVICE_MAINTAINERS="Trafalgar Square"
+   DEVICE_MAINTAINERS="Govind Pathak"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
